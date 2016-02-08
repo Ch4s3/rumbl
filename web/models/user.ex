20347a76fa600fb8c4d3f1f12b33e70593259c1a
Chase <chase.gilliam@gmail.com>
2016-02-08 00:28:57 -0500

    adding auth

--- web/models/user.ex
+++ web/models/user.ex
@@ -8,4 +8,27 @@ defmodule Rumbl.User do
     field :password_hash, :string
     timestamps
   end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 3, max: 23)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 8, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+    _ ->
+      changeset
+    end
+  end
 end
