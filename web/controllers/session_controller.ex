97ebd4f20c7d3aa73cd2e388c185bc8da1785e64
Chase <chase.gilliam@gmail.com>
2016-03-28 00:06:23 -0400

    up to ch6 and video generation

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -15,4 +15,11 @@ defmodule Rumbl.SessionController do
         |> put_flash(:error, "Invalid username/password combination") |> render("new.html")
     end
   end
+
+  def delete(conn, _) do
+    conn
+    |> Rumbl.Auth.logout()
+    |> put_flash(:info, "You have successfully logged out.")
+    |> redirect(to: page_path(conn, :index))
+  end
 end
